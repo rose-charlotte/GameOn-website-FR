637b25c0fed6f192e33927b123c8faced18593b9
zerossama <charlotte@anros.fr>
2023-08-10 19:38:24 +0200

    mise en place js pour form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,8 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
+const form = document.querySelector(".form");
+const btnSubmit = document.querySelector(".btn-submit");
 
 // launch modal event
 modalBtn.forEach(btn => btn.addEventListener("click", launchModal));
@@ -27,4 +29,26 @@ function launchModal() {
 // close modal form
 function closeModal() {
     modalbg.style.display = "none";
+}
+
+const validate = () => {
+    console.log("je valide");
+    btnSubmit.addEventListener("click", () => (modalbg.style.display = "none"));
+};
+//launch form event
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    const firstName = document.querySelector("#first").value;
+    const lastName = document.querySelector("#last").value;
+    const email = document.querySelector("#email").value;
+    const birthdate = document.querySelector("#birthdate").value;
+    const quantity = document.querySelector("#quantity").value;
+    console.log(firstName.length);
+
+    if (firstName.length >= 3) {
+        validate();
+    }
 }
