9ed7123e54f55dbbc718691ba2b0ce085a4eb457
zerossama <charlotte@anros.fr>
2023-08-28 19:10:01 +0200

    correction bug

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -23,9 +23,7 @@ const birthdate = document.querySelector("#birthdate");
 const quantity = document.querySelector("#quantity");
 const locationRadios = document.querySelectorAll("input[name='location']");
 const checkBoxOne = document.querySelector("#checkbox1");
-
 const btnSubmit = document.querySelector(".btn-submit");
-
 const heroSection = document.querySelector(".hero-section");
 const topNav = document.querySelector(".topnav");
 const footer = document.querySelector("footer");
@@ -57,7 +55,7 @@ function closeModal() {
     modalbg.style.display = "none";
 }
 
-//close modal inform inscription ok
+// close modal inform inscription ok
 function closeCongratsModal() {
     congrats.style.display = "none";
     heroSection.style.display = "grid";
@@ -65,8 +63,8 @@ function closeCongratsModal() {
     footer.style.display = "block";
 }
 
-//validation de chaque input:
-// fonction pour factoriser chaque validation d'input:
+// validation de chaque input:
+// fonction qui permet de faire apparaitre les messages d'erreurs reprise pour chaque validation d'input:
 const validateField = (validation, fieldId, messageOverride) => {
     const isValid = validation;
 
@@ -78,21 +76,20 @@ const validateField = (validation, fieldId, messageOverride) => {
     return isValid;
 };
 
-//Prénom
+// Prénom
 const validateFirstname = () => validateField(firstName.value && firstName.value.length >= 2, "first");
 
-//Nom
+// Nom
 const validateLastname = () => validateField(lastName.value && lastName.value.length >= 2, "last");
 
-//Email
-const validateMail = () => {
-    //utilisation d'un regex afin d'avoir un email valide (qui contient @ . et un minimum de caractères avant et apres le dot)
-    const validEmailRegex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+// Email
+// utilisation d'une regex afin d'avoir un email valide
+// Caractères alphanumériques, underscore, tiret et point suivi d'un arobase puis caractères alphanumériques, underscore, tiret et point puis un point suivi de caractères alphanumériques, underscore, tiret et point
+const validEmailRegex = /^[\w-\.]+@[\w-\.]+\.[\w-]+$/;
 
-    validateField(email.value.match(validEmailRegex), "email");
-};
+const validateMail = () => validateField(validEmailRegex.test(email.value), "email");
 
-//date de naissance
+// date de naissance
 const validateBirthDate = () => {
     const dateOfBirth = new Date(birthdate.value);
     const dateOfBirthWithoutTime = new Date(dateOfBirth.getFullYear(), dateOfBirth.getMonth(), dateOfBirth.getDate());
@@ -107,26 +104,23 @@ const validateBirthDate = () => {
     );
 };
 
-//nombre de tournois
-const validateQuantity = () => {
-    // Ici nous validons que l'input contient un entier avec 1 ou 2 chiffres uniquement
-    // Un input type number peut contenir les caractères suivants: + - e (notation scientifique)
-    const validNumberRegex = /^\d{1,2}$/;
-    validateField(quantity.value.match(validNumberRegex), "quantity");
-};
+// nombre de tournois
+// Ici nous validons que l'input contient un entier avec 1 ou 2 chiffres uniquement
+// Un input type number peut contenir les caractères suivants: + - e (notation scientifique)
+const validNumberRegex = /^\d{1,2}$/;
 
-//quel tournois
-const validateLocationRadio = () => {
-    // nous transformons la nodelist des radio buttons en un array afin d'itérer dessus
-    const validLocation = Array.from(locationRadios).some(radio => radio.checked);
+const validateQuantity = () => validateField(validNumberRegex.test(quantity.value), "quantity");
 
-    validateField(validLocation, "location1");
-};
+// quel tournois
+// nous transformons la nodelist des radio buttons en un array afin d'itérer dessus
+const validateLocationRadio = () =>
+    validateField(
+        Array.from(locationRadios).some(radio => radio.checked),
+        "location1"
+    );
 
-//checkbox: nous vérifions que la checkbox obligatoire est bien coché
-const validateCheckboxOne = () => {
-    validateField(checkBoxOne.checked, "checkbox1");
-};
+// checkbox: nous vérifions que la checkbox obligatoire est bien coché
+const validateCheckboxOne = () => validateField(checkBoxOne.checked, "checkbox1");
 
 // On accroche un listener "blur" sur les input qui permet de vérifier au fure et à mesure qu'ils sont valides afin d'afficher ou de cacher l'erreur pour l'utilisateur.
 firstName.addEventListener("blur", validateFirstname);
@@ -139,7 +133,8 @@ quantity.addEventListener("blur", validateQuantity);
 function validate(e) {
     e.preventDefault();
 
-    // consition d'envoie: nous appellons chaque fonction qui valide les input
+    // condition d'envoie: nous appellons chaque fonction qui valide les input
+
     let formIsValid = validateFirstname();
     formIsValid &= validateLastname();
     formIsValid &= validateMail();
@@ -148,7 +143,7 @@ function validate(e) {
     formIsValid &= validateLocationRadio();
     formIsValid &= validateCheckboxOne();
 
-    //si toute les fonctions de validation sont à true, alors on valide le formulaire et on renvoie vers le message de validation
+    // si toute les fonctions de validation sont à true, alors on valide le formulaire et on renvoie vers le message de validation
     if (formIsValid) {
         closeModal();
         if (window.screen.width < 800) {
